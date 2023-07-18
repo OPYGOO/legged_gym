b473bda3d9f570eb4b7fbfac9e1ce4018c6586a4
Musk Zao <1399803321@qq.com>
2023-07-18 18:45:57 +0800

    add wheel wheeled.py

---
+++ legged_gym/envs/four_wheeled/wheeled.py
@@ -0,0 +1,52 @@
+"""
+
+from legged_gym import LEGGED_GYM_ROOT_DIR, envs
+from time import time
+from warnings import WarningMessage
+import numpy as np
+import os
+
+from isaacgym.torch_utils import import *
+from isaacgym import gymtorch, gymapi, gymutil
+
+import torch
+from  torch import Tensor
+from typing import Tuple,Dict
+
+from legged_gym import LEGGED_GYM_ROOT_DIR
+from legged_gym.envs.base.base_task import BaseTask
+from legged_gym.utils.terrain import Terrain
+from legged_gym.utils.math import quat_apply_yaw, wrap_to_pi, torch_rand_sqrt_float
+from legged_gym.utils.helpers import class_to_dict
+from .four_wheeled_config import FourWheeledCfg
+class Wheeled(BaseTask):
+    def __int__(self, cfg: FourWheeledCfg, sim_params, physics_engine, sim_device, headless):
+        self.cfg = cfg
+        self.sim_params = sim_params
+        self.height_samples = None
+        self.debug_viz = False
+        self.init_done = False
+        self._parse_cfg(self.cfg)
+        super().__int__(self.cfg, sim_params, physics_engine, sim_device, headless)
+        if not self.headless:
+            self.set_camera(self.cfg.viewer.pos, self)
+"""
+from time import time
+import numpy as np
+import os
+
+from isaacgym.torch_utils import *
+from isaacgym import gymtorch, gymapi, gymutil
+
+import torch
+from typing import Tuple, Dict
+from legged_gym.envs import LeggedRobot
+
+class Wheeled(LeggedRobot):
+    def _reward_no_fly(self):
+        contacts = self.contact_forces[:, self.feet_indices, 2] > 0.1
+        single_contact = torch.sum(1.*contacts, dim=1)==1
+        return 1.*single_contact
+
+
+
