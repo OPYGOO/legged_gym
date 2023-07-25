602375246234c12be31cecc21026e1f9148bb3ed
Musk Zao <1399803321@qq.com>
2023-07-25 18:55:44 +0800

    add wheel wheeled.py

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -38,14 +38,18 @@ from .anymal_b.anymal_b_config import AnymalBRoughCfg, AnymalBRoughCfgPPO
 from .cassie.cassie import Cassie
 from .cassie.cassie_config import CassieRoughCfg, CassieRoughCfgPPO
 from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
+from .four_wheeled.four_wheeled_config import FourWheeledCfg, FourWheeledCfgPPO
+from .four_wheeled.wheeled import Wheeled
 
 
 import os
 
 from legged_gym.utils.task_registry import task_registry
 
+
 task_registry.register( "anymal_c_rough", Anymal, AnymalCRoughCfg(), AnymalCRoughCfgPPO() )
 task_registry.register( "anymal_c_flat", Anymal, AnymalCFlatCfg(), AnymalCFlatCfgPPO() )
 task_registry.register( "anymal_b", Anymal, AnymalBRoughCfg(), AnymalBRoughCfgPPO() )
 task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
 task_registry.register( "cassie", Cassie, CassieRoughCfg(), CassieRoughCfgPPO() )
+task_registry.register( "four_wheeled", Wheeled, FourWheeledCfg(), FourWheeledCfgPPO() )
