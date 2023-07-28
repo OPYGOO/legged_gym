f9e320feebdc6ecab72d94631d011eec65772e73
Musk Zao <1399803321@qq.com>
2023-07-28 19:57:04 +0800

    add wheel wheeled.py

--- legged_gym/envs/four_wheeled/four_wheeled_config.py
+++ legged_gym/envs/four_wheeled/four_wheeled_config.py
@@ -53,14 +53,14 @@ class FourWheeledCfg( LeggedRobotCfg ):
         decimation = 4
 
     class rewards( LeggedRobotCfg.rewards ):
-        soft_dof_pos_limit = 0.5
-        soft_dof_vel_limit = 0.5
+        soft_dof_pos_limit = 0.95
+        soft_dof_vel_limit = 0.95
         soft_torque_limit = 0.5
         only_positive_rewards = False
         base_height_target = 2.
         class scales( LeggedRobotCfg.rewards.scales ):
             base_height = -1.
-            orientation = -2.
+            #orientation = -1.
 
 class FourWheeledCfgPPO( LeggedRobotCfgPPO ):
         class runner( LeggedRobotCfgPPO.runner):
