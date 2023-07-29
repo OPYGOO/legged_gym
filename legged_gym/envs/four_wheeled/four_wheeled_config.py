f8d58d760fb64297a2258322932a332f63203061
Musk Zao <1399803321@qq.com>
2023-07-30 02:37:45 +0800

    add wheel wheeled.py

--- legged_gym/envs/four_wheeled/four_wheeled_config.py
+++ legged_gym/envs/four_wheeled/four_wheeled_config.py
@@ -22,7 +22,7 @@ class FourWheeledCfg( LeggedRobotCfg ):
 
 
     class init_state( LeggedRobotCfg.init_state ):
-        pos = [0.0, 0.0, 2] # x,y,z [m]
+        pos = [0.0, 0.0, 0.4] # x,y,z [m]
         default_joint_angles = { # = target angles [rad] when action = 0.0
             "LF_HAA": 0.0,
             "LH_HAA": 0.0,
